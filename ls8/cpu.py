1c9b29b749a2535bc18d4cb623fa1d52ef975a70
adrianbparra <adrianplane12@gmail.com>
2020-12-04 20:44:42 -0700

    Day 2:
    Added function to load files. Cleaned up run function to use O(1) when selecting a command to run. Implemented the multiply intruction.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,28 +14,48 @@ class CPU:
         self.LDI = 0b10000010
         self.PRN = 0b01000111
         self.HLT = 0b00000001
+        self.MUL = 0b10100010
+        self.branchtable = {}
+        self.branchtable[self.LDI] = self.handle_LDI
+        self.branchtable[self.PRN] = self.handle_PRN
+        self.branchtable[self.HLT] = self.handle_HLT
+        self.branchtable[self.MUL] = self.handle_MUL
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            if len(sys.argv) < 2:
+                print(f'Usage: python3 {sys.argv[0]} <filename>')
+                print("Exiting Program")
+                sys.exit(1)
+            
+            address = 0
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+            with open(f"C:\\Users\\PC1\\Documents\\Lambda_School\\Computer_Architecture\\Computer-Architecture\\ls8\\examples\\{sys.argv[1]}") as f:
+                print(f"Running file '{sys.argv[1]}'")
+                print("--------------------------------")
 
+                for line in f:
+                    split_line = line.split("#")[0]
+                    stripped_line = split_line.strip()
+                    if stripped_line != "":
+                        command = int(stripped_line, 2)
+
+                        # print(command)
+
+                        self.ram[address] = command
+                        address += 1
+
+        except FileNotFoundError:
+
+            print(f"Error file '{sys.argv[1]}' not found")
+            print("Exiting Program")
+            sys.exit(1)
+            
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -43,6 +63,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -52,14 +74,14 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        print(f"TRACE: %02X | RAM: %02X %02X %02X |" % (
             self.pc,
             #self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
-        ), end='')
+        ), end='REG:')
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
@@ -71,35 +93,52 @@ class CPU:
         return self.ram[address]
 
     def ram_write(self,value,address):
-        pass
+        
         self.reg[address] =  value
+
+    def handle_LDI(self):
+        
+        address = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+
+        self.ram_write(value,address)
+
+        self.pc += 2
+
+    def handle_PRN(self):
+
+        address = self.ram_read(self.pc + 1)
+
+        print(self.reg[address])
+
+        self.pc += 1
+
+    def handle_MUL(self):
+        
+        registerA = self.ram_read(self.pc + 1)
+        registerB = self.ram_read(self.pc + 2)
+
+        value = self.alu("MUL", registerA, registerB)
+
+        self.pc += 2
+        
+    def handle_HLT(self):
+
+        self.running = False
 
     def run(self):
         """Run the CPU."""
-
+        
         while self.running:
+            # self.trace()
             
             command = self.ram[self.pc]
 
-            if command == self.LDI:
-                # set value of a reg to an int
-                value = self.ram_read(self.pc + 2)
+            if command in self.branchtable:
 
-                address = self.ram_read(self.pc + 1)
-
-                self.ram_write(value, address)
-
-
-                self.pc += 2
-
-            elif command == self.PRN:
-                
-                address = self.ram_read(self.pc + 1)
-
-                print(self.reg[address])
-
-                self.pc += 1
-            elif command == self.HLT:
-                self.running = False
+                self.branchtable[command]()
+            else:
+                print(f"Error {bin(command)} not Found")
+            
 
             self.pc += 1
